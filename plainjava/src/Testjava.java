68afc0f4bd78e754b1e88abeea2b0db363138139
sudhakar4782 <sudhakar4782@gmail.com>
2021-01-26 14:42:45 +0530

    change in master

--- plainjava/src/Testjava.java
+++ plainjava/src/Testjava.java
@@ -2,5 +2,6 @@ public class Testjava {
     public static void main(String[] args) {
         MyMath math = new MyMath();
         System.out.println("Result is: " + math.multi(5, 10));
+		System.out.println("This is master.");
     }
 }
